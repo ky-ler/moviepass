ec23257c0ac4ad4c66d0722fd93c77a539483066
Kyler Riggs <100742516+KylerRiggs@users.noreply.github.com>
2022-09-01 00:48:20 -0400

    Re-add console log

--- server.js
+++ server.js
@@ -45,5 +45,5 @@ app.use('/movies', movieRoutes)
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log('Server is running, you better catch it!')
-    console.log(`Server running on http://localhost:${PORT}`)
+    // console.log(`Server running on http://localhost:${PORT}`)
 })
